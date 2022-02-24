8e49529d52d1d87f22d0c786ccc3c0f3048a3c08
amber.zhang <774524217@qq.com>
2022-02-24 10:24:00 +0800

    fix test

--- test.py
+++ test.py
@@ -42,6 +42,10 @@ if __name__ == '__main__':
         predictions = model.predict(inpt)    # list of [b,h,w,a,c+1+4]
 
         nL = len(predictions)
+        boxes_xcyc_postives_all_level = []  # abs
+        boxes_wh_postives_all_level = []   # abs
+        conf_postives_all_level = []
+        probs_postives_all_level = []
         for i in range(nL):
             grid_h = grid_w = img_size//strides[i]
             pred = np.reshape(predictions[i], (grid_h,grid_w,n_anchors,n_classes+1+4))
@@ -67,38 +71,49 @@ if __name__ == '__main__':
             boxes_xcyc_postives = (boxes_xcyc_postives+grid_positives) * strides[i]
             boxes_wh_postives = boxes_wh_postives * anchor_positives
 
-            # run nms
-            boxes_x1y1 = boxes_xcyc_postives - boxes_wh_postives/2.
-            boxes_x2y2 = boxes_xcyc_postives + boxes_wh_postives/2.
-            boxes = np.concatenate([boxes_x1y1,boxes_x2y2],axis=1)
-            scores = conf_postives * np.max(probs_positives, axis=1)
-            labels = np.argmax(probs_positives, axis=1)
-            if len(boxes):
-                boxes, scores, labels = hard_nms(boxes,scores,labels,score_thresh=0.1,iou_thresh=0.3)
+            boxes_xcyc_postives_all_level.append(boxes_xcyc_postives)
+            boxes_wh_postives_all_level.append(boxes_wh_postives)
+            conf_postives_all_level.append(conf_postives)
+            probs_postives_all_level.append(probs_positives)
 
-                # iter boxes
-                f = open(os.path.join('preds/', file.replace('.jpg','.txt')), 'w')
-                for b in range(len(boxes)):
-                    x1,y1,x2,y2 = boxes[b]
-                    prob = scores[b]
-                    label = labels[b]
+        # merge all level
+        boxes_xcyc_postives = np.concatenate(boxes_xcyc_postives_all_level, axis=0)
+        boxes_wh_postives = np.concatenate(boxes_wh_postives_all_level, axis=0)
+        conf_postives = np.concatenate(conf_postives_all_level, axis=0)
+        probs_positives = np.concatenate(probs_postives_all_level, axis=0)
 
-                    normed_xc = (x1+x2)/2./(img_size-pad_w)
-                    normed_yc = (y1+y2)/2./(img_size-pad_h)
-                    normed_w = (x2-x1)/(img_size-pad_w)
-                    normed_h = (y2-y1)/(img_size-pad_h)
-                    f.write(' '.join(map(str, [label,normed_xc,normed_yc,normed_w,normed_h])))
+        # run nms across level
+        boxes_x1y1 = boxes_xcyc_postives - boxes_wh_postives/2.
+        boxes_x2y2 = boxes_xcyc_postives + boxes_wh_postives/2.
+        boxes = np.concatenate([boxes_x1y1,boxes_x2y2],axis=1)
+        scores = conf_postives * np.max(probs_positives, axis=1)
+        labels = np.argmax(probs_positives, axis=1)
+        if len(boxes):
+            boxes, scores, labels = hard_nms(boxes,scores,labels,score_thresh=0.1,iou_thresh=0.3)
 
-                    # draw box
-                    cv2.rectangle(cv_img, (int(x1),int(y1)), (int(x2),int(y2)), (0,255,255), 3)
-                    pil_image = Image.fromarray(cv_img.astype(np.uint8))
-                    label = '{} {:.2f}'.format(classes[label], prob)
-                    draw = ImageDraw.Draw(pil_image)
-                    font = ImageFont.truetype('msyh.ttc', size=np.floor(3e-2*pil_image.size[1]+0.5).astype('int32'))
-                    draw.text((int(x1),int(y1)), label, fill='blue', font=font)
-                    cv_img = np.array(pil_image)
+            # iter boxes
+            f = open(os.path.join('preds/', file.replace('.jpg','.txt')), 'w')
+            for b in range(len(boxes)):
+                x1,y1,x2,y2 = boxes[b]
+                prob = scores[b]
+                label = labels[b]
 
-                f.close()
+                normed_xc = (x1+x2)/2./(img_size-pad_w)
+                normed_yc = (y1+y2)/2./(img_size-pad_h)
+                normed_w = (x2-x1)/(img_size-pad_w)
+                normed_h = (y2-y1)/(img_size-pad_h)
+                f.write(' '.join(map(str, [label,normed_xc,normed_yc,normed_w,normed_h])))
+
+                # draw box
+                cv2.rectangle(cv_img, (int(x1),int(y1)), (int(x2),int(y2)), (0,255,255), 3)
+                pil_image = Image.fromarray(cv_img.astype(np.uint8))
+                label = '{} {:.2f}'.format(classes[label], prob)
+                draw = ImageDraw.Draw(pil_image)
+                font = ImageFont.truetype('msyh.ttc', size=np.floor(3e-2*pil_image.size[1]+0.5).astype('int32'))
+                draw.text((int(x1),int(y1)), label, fill='blue', font=font)
+                cv_img = np.array(pil_image)
+
+            f.close()
 
         cv2.imwrite('preds/%s' % file, cv_img)
 
